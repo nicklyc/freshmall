50dff97b3b855e887a672178331ddf3d8a6cf1f4
maochao <maochao@tongtech.com>
2021-04-26 11:41:29 +0800

    初始化

--- kxmall-data/src/main/java/com/kxmall/market/data/mapper/OutStockSpuMapper.java
+++
@@ -1,7 +0,0 @@
-package com.kxmall.market.data.mapper;
-
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.kxmall.market.data.domain.OutStockSpuDO;
-
-public interface OutStockSpuMapper extends BaseMapper<OutStockSpuDO> {
-}
